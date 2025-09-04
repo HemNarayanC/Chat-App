861f0e74f0519e52d5e2ef4fceca018d019799bc
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-04 12:54:19 +0545

    login logic with token generation

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,10 +1,11 @@
+import generateToken from "../lib/jwtToken";
 import User from "../models/user.model";
 import bcrypt from 'bcrypt'
 
 const signup = async (req, res) => {
     try {
-        const { name, email, password } = req.body;
-        if (!name || !email || !password) {
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
             return res.status(400).json({
                 success: false,
                 message: "All fields are required"
@@ -24,7 +25,7 @@ const signup = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = await User.create({
-            name,
+            username,
             email,
             password: hashedPassword
         });
@@ -42,6 +43,46 @@ const signup = async (req, res) => {
             message: "Internal Server Error. User not created.",
             newUser
         });
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "email and password are required to login"
+            });
+        }
+
+        const user = await User.findOne({email}).select('-password');
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Credentials"
+            });
+        }
+
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        console.log(isPasswordCorrect);
+        if(!isPasswordCorrect){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Credentials"
+            });
+        }
+
+        generateToken({userId: user._id, email});
+        res.status(200).json({
+           success: true,
+           message: "logged in successfully",
+           user
+        });
+
+    } catch (error) {
+
     }
 }
 
