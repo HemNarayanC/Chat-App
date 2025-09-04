5c2bcc12b51780f389e7015a8be403928c08daf2
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-04 11:55:59 +0545

    user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      default: "default.png",
+    }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
