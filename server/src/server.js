f962ef91fd2957c2653c9d3c156aece26347d623
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-05 09:48:20 +0545

    cookie parser and db method

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import authRoute from './routes/auth.routes.js';
+import connectDB from './database/db_connection.js';
 const app = express();
 
 dotenv.config();
+connectDB();
 
+app.use(cookieParser());
 app.use(express.json());
+
+app.use('/api/auth', authRoute);
 
 
 const PORT = process.env.PORT;
