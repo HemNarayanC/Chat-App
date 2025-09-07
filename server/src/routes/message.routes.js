feb4845563a86bd28462da4fa76e9717a1dcbde5
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-07 20:48:21 +0545

    Add route for getting messages between sender and receiver

--- server/src/routes/message.routes.js
+++ server/src/routes/message.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import protectRoute from '../middlewares/auth.middlware';
+import { getMessages } from '../controllers/message.controller';
 
 const router = express.Router();
 
 router.get('/users', protectRoute, getUsersForSidebar);
+router.get('/:id', protectRoute, getMessages);
 
 export default router
