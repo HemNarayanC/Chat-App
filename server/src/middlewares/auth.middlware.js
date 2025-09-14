8f5672c6451ee1d7928ae8555e7077a18eff09af
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-14 11:15:27 +0545

    auth middleware

--- server/src/middlewares/auth.middlware.js
+++ server/src/middlewares/auth.middlware.js
@@ -2,22 +2,23 @@ import jwt from 'jsonwebtoken'
 const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.token;
-        if(!token){
+        if (!token) {
             return res.status(401).json({
                 success: false,
                 message: "Unauthorized - no token provided or token expired."
             });
         }
 
-        const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-        if(!decoded){
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (!decoded) {
             return res.status(401).json({
                 success: false,
                 message: "Unauthorized - Invalid token."
             });
         }
-
+        // console.log(decoded);
         req.user = decoded;
+        next();
     } catch (error) {
         console.log("Error ", error);
         return res.status(500).json({
