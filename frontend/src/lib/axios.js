7e86053c5a9855ae269d31bd2d88e3e18f493493
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-14 19:43:42 +0545

    fix: correctly set axios baseURL for dev and production environments

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_SERVER_URL}` : "/"
 
 const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
+    baseURL: BASE_URL,
     withCredentials: true
 });
 
