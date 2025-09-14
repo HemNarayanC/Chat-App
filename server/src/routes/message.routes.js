6d0b09ce3e9465c2209b21a3822d35b6bb94c3b8
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-14 08:00:55 +0545

    add route for sending message with protectRoute

--- server/src/routes/message.routes.js
+++ server/src/routes/message.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import protectRoute from '../middlewares/auth.middlware';
-import { getMessages } from '../controllers/message.controller';
+import protectRoute from '../middlewares/auth.middlware.js';
+import { getMessages, getUsersForSidebar, sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 router.get('/users', protectRoute, getUsersForSidebar);
 router.get('/:id', protectRoute, getMessages);
+router.post('/send/:id', protectRoute, sendMessage);
 
 export default router
