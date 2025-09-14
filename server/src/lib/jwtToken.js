c4f62c65ee6b5798c1ab96cc47e38f588a3ed659
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-14 11:13:04 +0545

    improve JWT cookie settings for production environment

--- server/src/lib/jwtToken.js
+++ server/src/lib/jwtToken.js
@@ -1,15 +1,18 @@
 import jwt from 'jsonwebtoken'
 
-const generateToken = ({userId, email}, res) => {
-    const accessToken = jwt.sign({userId, email}, process.env.JWT_SECRET_KEY, {
+const generateToken = ({ userId, email }, res) => {
+    const accessToken = jwt.sign({ userId, email }, process.env.JWT_SECRET_KEY, {
         expiresIn: '1h'
     });
 
+    const isProd = process.env.NODE_ENV === 'production';
+
+    console.log("Mode ", process.env.NODE_ENV)
     res.cookie('token', accessToken, {
-        httpOnly:  true,
-        secure: process.env.NODE_ENV == 'production',
-        sameSite: 'strict',
-        maxAge: 24*60*60*1000
+        httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? 'none' : 'lax',
+        maxAge: 24 * 60 * 60 * 1000
     });
 
     return accessToken;
