39906f18c5366a7389b9c78c2ef17e0e392b1410
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-14 08:02:22 +0545

    add socket io implementation

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import Message from '../models/message.model.js';
+import { getReceiverSocketId, io } from '../lib/socket.js';
 
 const getUsersForSidebar = async (req, res) => {
     try {
@@ -65,6 +66,10 @@ const sendMessage = async (req, res) => {
         await newMessage.save();
 
         // todo: socket.io implementation
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(200).json({
             success: true,
